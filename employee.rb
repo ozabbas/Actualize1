d1764a6285b99f4f40a52732037782fabd12aa8b
Owais Abbas <ozabbas95@gmail.com>
2021-06-08 18:33:34 -0500

    changes made to employee class

--- employee.rb
+++ employee.rb
@@ -1,14 +1,17 @@
 class Employee
+  
   def initialize(input_first_name, input_last_name, input_active, input_salary)
     @first_name = input_first_name
     @last_name = input_last_name
     @active = input_active
     @salary = input_salary
   end
-end
 
-def print_info
-  p "#{@first_name} #{@last_name} makes $#{@salary}"
+  def print_info
+  p "#{@first_name} #{@last_name} makes $#{@salary} per year"
+  end
+
+end
 
 employee1 = Employee.new("Majora", "Carter", true, 50000)
 employee1.print_info
